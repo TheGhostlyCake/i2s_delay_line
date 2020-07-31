b180f083d3fa7f133f031858294b72f433b477f6
tom charter <mrtbc101@gmail.com>
2020-07-31 23:29:04 +0100

    Adding fix for stupidly lazy commit

--- testbench/i2s_receiver_agent.vhd
+++ testbench/i2s_receiver_agent.vhd
@@ -9,7 +9,7 @@ use vunit_lib.logger_pkg.all;
 library i2s_freq_pkg;
 use i2s_freq_pkg.i2s_freq_pkg.all;
 
-entity i2s_generator_agent is 
+entity i2s_receiver_agent is 
 generic (
     bit_rate : integer := 441000;
     bit_depth : integer := 16
@@ -21,7 +21,7 @@ port (
 );
 end entity;
 
-architecture rtl of i2s_generator_agent is
+architecture rtl of i2s_receiver_agent is
  
     
 begin
